b1f45988d3cb81b8dceac14a04e5969c5c9a5ae4
JJEllacopulos <55036974+JJEllacopulos@users.noreply.github.com>
2019-10-15 20:15:52 -0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<windows.h>
+//E:\Mini laboratorio\Popurri\Partijuego\Cajon de sastre\Reprocuctor\DJPON1\Pistas
+int main(){
+
+    //system("D:\\Desktop\\01-Ash.wav");
+
+    ///Por alguna razon, los archivos dentro del pen no los reproduce si no es una ruta parcial.
+    //system("Pistas\\01-Ash.wav");
+
+    ///Las sigientes son con librerias:
+
+    ///El tutorial de donde lo saque es basura.
+    //sndPlaySound("D:\\Desktop\\01-Ash.wav", 0);
+
+    ///PlaySound("D:\\Desktop\\01-Ash.wav", NULL, SND_FILENAME);
+
+    return 0;
+}
